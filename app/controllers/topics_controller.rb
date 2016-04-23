976f63bfa4da351b0c580694cb03b781751fd244
Lexis <email": null>
2016-04-23 12:28:49 -0700

    12345!!

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -17,6 +17,12 @@ class TopicsController < ApplicationController
     @topic = Topic.new
   end
 
+  def upvote
+    @topic = Topic.find(params[:id])
+    @topic.votes.create
+    redirect_to(topics_path)
+  end
+
   # GET /topics/1/edit
   def edit
   end
@@ -28,7 +34,7 @@ class TopicsController < ApplicationController
 
     respond_to do |format|
       if @topic.save
-        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+        format.html { redirect_to topics_path, notice: 'Topic was successfully created.' }
         format.json { render :show, status: :created, location: @topic }
       else
         format.html { render :new }
@@ -42,7 +48,7 @@ class TopicsController < ApplicationController
   def update
     respond_to do |format|
       if @topic.update(topic_params)
-        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.html { redirect_to topics_path, notice: 'Topic was successfully updated.' }
         format.json { render :show, status: :ok, location: @topic }
       else
         format.html { render :edit }
